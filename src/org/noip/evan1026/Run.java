bf3bd03f3e6a1518902f451dd83f0af5c20f81fa
Evan Allan <kkrockme4@gmail.com>
2012-11-25 17:28:11 -0500

    Fixing Run Class
    Fixing (probably, at least) the problem where it starts to render before
    the form has been made.

--- src/org/noip/evan1026/Run.java
+++ src/org/noip/evan1026/Run.java
@@ -6,9 +6,9 @@ public class Run {
 	public static void main(String[] args) {
 		DrawingPanel form = new DrawingPanel();
 	    JFrame frame = new JFrame("Bouncy Balls");
+	    frame.setSize(600, 600);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.add(form);
-	    frame.setSize(600, 600);
 	    frame.setVisible(true);
 	}
 
